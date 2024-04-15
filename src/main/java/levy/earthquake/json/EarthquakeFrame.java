f129191031cea33937cad72820b31bdba0dc25d8
Yaffa Levy <yaffalevy@Yaffas-MacBook-Air.local>
2024-04-15 16:45:05 -0400

    added radio buttons

--- src/main/java/levy/earthquake/json/EarthquakeFrame.java
+++ src/main/java/levy/earthquake/json/EarthquakeFrame.java
@@ -2,6 +2,7 @@ package levy.earthquake.json;
 
 
 import hu.akarnokd.rxjava3.swing.SwingSchedulers;
+import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import levy.earthquake.EarthquakeService;
@@ -14,6 +15,7 @@ import java.util.Arrays;
 
 public class EarthquakeFrame extends JFrame {
     private JList<String> jlist = new JList<>();
+    private Disposable disposable;
 
     public EarthquakeFrame() {
         setSize(400, 600);
@@ -21,18 +23,48 @@ public class EarthquakeFrame extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        add(jlist, BorderLayout.CENTER);
+        JRadioButton oneHourRadioButton = new JRadioButton("One Hour");
+        JRadioButton thirtyDaysRadioButton = new JRadioButton("Thirty Days");
+
+        ButtonGroup radioButtonGroup = new ButtonGroup();
+        radioButtonGroup.add(oneHourRadioButton);
+        radioButtonGroup.add(thirtyDaysRadioButton);
+
+
+
+        JPanel radioButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        radioButtonPanel.add(oneHourRadioButton);
+        radioButtonPanel.add(thirtyDaysRadioButton);
+        add(radioButtonPanel, BorderLayout.NORTH);
+        add(new JScrollPane(jlist), BorderLayout.CENTER);
 
         EarthquakeService service = new EarthquakeServiceFactory().getService();
-        Disposable disposable = service.oneHour()
-                .subscribeOn(Schedulers.io())
+
+        oneHourRadioButton.addActionListener(e -> {
+            if (oneHourRadioButton.isSelected()) {
+                fetchEarthquakeData(service.oneHour());
+            }
+        });
+        thirtyDaysRadioButton.addActionListener(e -> {
+            if (thirtyDaysRadioButton.isSelected()) {
+
+                fetchEarthquakeData(service.significantLast30Days());
+            }
+        });
+    }
+
+    private void fetchEarthquakeData(Single<FeatureCollection> data) {
+
+        if ( disposable != null && !disposable.isDisposed()) {
+            disposable.dispose();
+        }
+
+        disposable = data.subscribeOn(Schedulers.io())
                 .observeOn(SwingSchedulers.edt())
                 .subscribe(
                         this::handleResponse,
                         Throwable::printStackTrace);
-
     }
-
     private void handleResponse(FeatureCollection response) {
         String[] listData = Arrays.stream(response.features)
                 .map(feature -> feature.properties.mag + " " + feature.properties.place)
@@ -40,7 +72,6 @@ public class EarthquakeFrame extends JFrame {
                 .toArray(new String[0]);
         jlist.setListData(listData);
     }
-
     public static void main(String[] args) {
         new EarthquakeFrame().setVisible(true);
 
